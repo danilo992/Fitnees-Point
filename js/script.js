e3f768ae5c77f89ed8c552b6671f296e917eb8bf
Danilo <tadeudanilo46@gmail.com>
2019-09-12 01:53:04 -0300

    progeto front-end

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function acao(){
+    var submenu = document.getElementById('submenu');
+        if (submenu.style.display == 'block'){
+            submenu.style.display = 'none';
+    } else {
+        submenu.style.display = 'block';
+    }
+}
